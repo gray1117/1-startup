68cb64a88cd54e7bd813e628324939656ad04379
Daniel Gray <47634911+gray1117@users.noreply.github.com>
2019-06-23 19:55:24 +0100

    Add files via upload

--- homework2.py
+++ homework2.py
@@ -2,37 +2,45 @@ from __future__ import print_function
 from __future__ import unicode_literals
 import math	
 
-class Work:
-	def __init__(self, n, h, b, o):
+class Work(object):
+	def __init__(self, n, h, b):
 		self.basehours = 40
 		self.morehours = 60
 		self.name = n
 		self.base = b
 		self.hours = h
-		self.overtime = o
+		
+	def calcOvertime(self): ###to calc overtime rate###
+		return self.base * 1.5
 
-	def calcExtra(self): ####this is to calc overtime pay###
+	def calcExtra(self): ####to calc overtime pay###
 		if self.morehours > self.hours >= self.basehours:
-			return (self.hours - self.basehours) * self.overtime
+			return (self.hours - self.basehours) * self.calcOvertime()
 		elif self.basehours >= self.hours:
 			return 0.00
 		else:
-			return 20 * self.overtime
+			return 20 * self.calcOvertime()
 
-	def calcEarnings(self):	 ####this is to calc total earnings###
+	def calcEarnings(self):	 ####to calc total earnings###
 		if self.basehours >= self.hours: 
 			return self.hours * self.base
 		elif self.morehours >= self.hours > self.basehours:
 			return (self.basehours * self.base) + self.calcExtra() 
 		else:
-			return (self.basehours * self.base) + (20 * self.overtime)
+			return (self.basehours * self.base) + (20 * self.calcOvertime())
 	
-j = Work("John",42,7.25,10.86)
-s = Work("Sully",20,8.40,12.60)
-c = Work("Chris",40,7.00,10.50)
-w = Work("Will",65,8.50,12.75)
+	def printOutput(self):
+		print (self.name,"has worked",self.hours,"hours this week and earned",u"\u00A3",
+		self.calcEarnings(),"including",u"\u00A3",self.calcExtra(),"overtime pay")
+			
+j = Work("John",42,7.25)
+l = Work("Lucy",48,7.50)
+s = Work("Sully",20,8.40)
+c = Work("Chris",40,7.00)
+w = Work("Will",65,8.50)
 
-print(j.name,"has worked", j.hours,"hours this week and earned",u"\u00A3",j.calcEarnings(),"including",u"\u00A3",j.calcExtra(),"overtime pay")
-print(s.name,"has worked", s.hours,"hours this week and earned",u"\u00A3",s.calcEarnings(),"including",u"\u00A3",s.calcExtra(),"overtime pay")
-print(c.name,"has worked", c.hours,"hours this week and earned",u"\u00A3",c.calcEarnings(),"including",u"\u00A3",c.calcExtra(),"overtime pay")
-print(w.name,"has worked", w.hours,"hours this week and earned",u"\u00A3",w.calcEarnings(),"including",u"\u00A3",w.calcExtra(),"overtime pay")
+j.printOutput()
+l.printOutput()
+s.printOutput()
+c.printOutput()
+w.printOutput()
